681a2f377f487d1a5576aedcfac4e86b38ff9ee5
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-16 20:23:36 +0530

    [akarsh] palindrome

---
+++ core-java-practice/leet-code-codebase/pali.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class pali {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        // Input
+        int n = x.nextInt();
+
+        if (n < 0) {
+            System.out.println("false");
+        } else {
+            int rev = 0;
+            int num = n;
+
+            while (num != 0) {
+                rev = rev * 10 + num % 10;
+                num = num / 10;
+            }
+
+            if (rev == n) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+
+        
+    }
+}
