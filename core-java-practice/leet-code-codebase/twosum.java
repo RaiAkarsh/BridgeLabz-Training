1180f4d79d5f978ab1a9372650d44b612b19137f
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-16 20:26:38 +0530

    [akarsh] two sum

---
+++ core-java-practice/leet-code-codebase/twosum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class twosum {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        // Input array size and elements
+        int n = x.nextInt();
+
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = x.nextInt();
+        }
+
+        // Input target
+        int t = x.nextInt();
+        int ar[] = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == t) {
+                    ar[0] = i;
+                    ar[1] = j;
+                }
+            }
+        }
+
+        // Output
+        System.out.println(ar[0] + " " + ar[1]);
+    }
+}
