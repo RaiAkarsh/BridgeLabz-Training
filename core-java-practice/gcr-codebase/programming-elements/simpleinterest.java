8ef5e825f5f1d082ff0ccaf1c5890121d6f33d9c
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-17 16:05:41 +0530

    [akarsh] calculate simple interest

---
+++ core-java-practice/gcr-codebase/programming-elements/simpleinterest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class simpleinterest{
+  public static void main(String []args){
+    Scanner x=new Scanner(System.in);
+    int p=x.nextInt();
+    int r=x.nextInt();
+    double t=x.nextInt();
+    double si= (p*r*t)/100;
+    System.out.print(si);
+  }
+}
