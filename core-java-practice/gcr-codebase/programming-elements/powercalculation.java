2cf656d0b0ab197a26cbd09350bfeaa06a3e44b0
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-17 16:34:57 +0530

    [akarsh] power calculation

---
+++ core-java-practice/gcr-codebase/programming-elements/powercalculation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class powercalculation{
+  public static void main(String []args){
+    Scanner x=new Scanner(System.in);
+    int b=x.nextInt();
+    int e=x.nextInt();
+    System.out.print((int)Math.pow(b,e));
+  }
+}
