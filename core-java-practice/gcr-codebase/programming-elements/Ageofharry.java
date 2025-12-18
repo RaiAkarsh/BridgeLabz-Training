c2d78a90963a44f8f5e68af2c9bd39aedfd57536
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-18 11:15:40 +0530

    [akarsh] Age of Harry

---
+++ core-java-practice/gcr-codebase/programming-elements/Ageofharry.java
@@ -0,0 +1,7 @@
+public class Ageofharry {
+    public static void main(String[] args) {
+        int cyear = 2024; 
+        int age = cyear - 2000;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
