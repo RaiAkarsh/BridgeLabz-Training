6af643a2bf50203ba160eeead161ec71ac70aa29
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-18 11:17:25 +0530

    [akarsh] Profit

---
+++ core-java-practice/gcr-codebase/programming-elements/Profitloss.java
@@ -0,0 +1,10 @@
+public class Profitloss {
+    public static void main(String[] args) {
+        int cp=129;
+        int sp=191;
+        int profit=sp-cp;
+        int profitp=(profit*100)/cp;
+        System.out.println("The Cost Price is INR "+cp +", Selling Price is INR "+sp+ "\n"+"The Profit is INR "+profit +", Profit Percentage is "+profitp);
+    }
+    
+}
