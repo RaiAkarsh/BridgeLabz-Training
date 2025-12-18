a3c74d2183ca803ae769770702979413370e73b9
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-18 14:35:22 +0530

    [akarsh] rename

--- core-java-practice/gcr-codebase/programming-elements/addtwonumbers.java
+++ core-java-practice/gcr-codebase/programming-elements/addtwonumbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Addtwonumbers{
+class AddTwoNumbers{
   public static void main(String []args){
     Scanner x=new Scanner(System.in);
     int a=x.nextInt();
--- core-java-practice/gcr-codebase/programming-elements/kilometrstomiles.java
+++ core-java-practice/gcr-codebase/programming-elements/kilometrstomiles.java
@@ -1,4 +1,4 @@
-public class kilometrstomiles {
+public class Kilometrstomiles {
     public static void main(String[] args) {
         double kilometers = 10.8;
         double miles = kilometers / 1.6;
