00eb88a16bfa039b58ba467c63f6f4d03002dccb
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-18 15:08:00 +0530

    [akarsh] rename addtwonumbers.java to AddTwoNumbers.java

--- core-java-practice/gcr-codebase/programming-elements/Ageofharry.java
+++ core-java-practice/gcr-codebase/programming-elements/Ageofharry.java
@@ -1,5 +1,6 @@
 public class Ageofharry {
     public static void main(String[] args) {
+        
         int cyear = 2024; 
         int age = cyear - 2000;
         System.out.println("Harry's age in 2024 is " + age);
--- core-java-practice/gcr-codebase/programming-elements/Welcometobridgelabz.java
+++ core-java-practice/gcr-codebase/programming-elements/Welcometobridgelabz.java
@@ -1,4 +1,4 @@
-class Welcometobridgelabz{
+class WelcomeToBridgelabz{
 
   public static void main(String []args){
     System.out.print("Welcome to Bridgelabz");
