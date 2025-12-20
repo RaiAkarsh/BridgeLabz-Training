9dc58949ceb2e1262c36ef82e708cd95bdf22e8f
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-20 12:28:54 +0530

    [akarsh] Add.CoffeeCounter in scenario-based

---
+++ core-java-practice/scenario-based/CoffeeCounter.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class CoffeeCounter{
+    public static void main(String[] args){
+       Scanner x=new Scanner(System.in);
+       String coffeeType;
+         int quantity;
+         while(true){
+            System.out.println("Enter coffee type (Espresso/Latte/Cappuccino) or 'exit' to finish:");
+            coffeeType=x.next().toLowerCase();
+            if(coffeeType.equals("exit")){
+                break;
+            }
+            System.out.println("Enter quantity:");
+            quantity=x.nextInt();
+            double price=0.0;
+            switch ((coffeeType)) {
+                case "espresso":
+                    price=150;
+                    break;
+                case "latte":
+                    price=200;
+                    break;
+                case "cappuccino":
+                    price=250;
+                    break;
+                default:
+                    System.out.println("Invalid coffee type. Please try again.");
+                    continue;        
+            }
+            double bill=price*quantity;
+            System.out.println("Bill amount: " + bill);
+            double gst=bill*0.18;
+            System.out.println("GST (18%): " + gst);
+            double totalAmount=bill+gst;
+            System.out.println("Total amount to be paid: " + totalAmount);
+         }
+    }
+}
