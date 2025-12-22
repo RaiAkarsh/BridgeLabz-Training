fd322409cd35af0108026fb2e7c8f828e96d8786
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-22 22:56:46 +0530

    [akarsh] Add. word length

---
+++ core-java-practice/gcr-codebase/strings/WordsLength.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class WordsLength {
+
+    public static int findLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+            return count;
+        }
+    }
+
+    public static String[] manualSplit(String str) {
+        return str.split(" ");
+    }
+
+    public static String[][] wordLength(String[] words) {
+
+        String[][] table = new String[words.length][2];
+
+        for (int i = 0; i < words.length; i++) {
+            table[i][0] = words[i];
+            table[i][1] = String.valueOf(findLength(words[i]));
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String text = x.nextLine();
+
+        String[] words = manualSplit(text);
+        String[][] table = wordLength(words);
+
+        System.out.println("Word\tLength");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(table[i][0] + "\t" + Integer.parseInt(table[i][1]));
+        }
+    }
+}
