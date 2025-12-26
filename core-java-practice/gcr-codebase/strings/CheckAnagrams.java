78ab8f12ff5abe18e45d36fefc9a80f4e2781a16
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-26 16:51:23 +0530

    [akarsh] Add. Check Anagrams Program

---
+++ core-java-practice/gcr-codebase/strings/CheckAnagrams.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class CheckAnagrams {
+    public static void main(String[] args) {
+            Scanner x = new Scanner(System.in);
+            System.out.println("Enter the first string and second string");
+            String str1 = x.nextLine().toLowerCase().replace(" ", "");
+            String str2 = x.nextLine().toLowerCase().replace(" ", "");
+            if (str1.length() != str2.length()) {
+                System.out.println("Strings are not anagrams.");
+                return;
+            }
+            String str11 = sortstr(str1);
+            String str22 = sortstr(str2);
+            if (str11.equals(str22)) {
+                
+                System.out.println("Strings are anagrams.");
+            }
+            else {
+                System.out.println("Strings are not anagrams.");
+            }
+        }
+        public static String sortstr(String str) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            return new String(ch);
+        }
+}
