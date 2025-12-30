688858456b6cc5d761c39364cb98bfd9c553c7fe
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 15:12:08 +0530

    [akarsh] Refactor. uc3 check option no play/ladder/snake

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -7,6 +7,18 @@ public class SnakeAndLadder {
 		
 		int diceNumber = (int)(Math.random()*6)+1;
 		System.out.println("Dice Number "+  diceNumber);
+		
+		int option = (int)(Math.random()*3)+1;
+		if(option==1) {
+			System.out.println("No Play ");
+		}
+		else if(option==2) {
+			position+=diceNumber;
+			System.out.println("Ladder Move to "+ position);
+		}
+		else {
+			position-=diceNumber;
+			System.out.println("Snake Move to "+ position);
+		}
 	}
-
 }
