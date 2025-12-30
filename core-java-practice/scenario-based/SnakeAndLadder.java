56256b4e32ec24a2a7dfa89e824c60b009fd10c4
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 15:36:07 +0530

    [akarsh] Refactor. uc6 Report the number of times the dice was played to win the game

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -4,14 +4,14 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game");
 		System.out.println("Start position is 0");
-		int position = 0;
+		int position = 0,dicecount=0;;
 		while (position < 100) {
 			int diceNumber = (int) (Math.random() * 6) + 1;
 			System.out.println("Dice Number " + diceNumber);
 
 			int option = (int) (Math.random() * 3) + 1;
 			if (option == 1 || (position + diceNumber)>100) {
-				System.out.println("No Play ");
+				System.out.println("No Play Stay at "+ position);
 			} 
 			else if (option == 2) {
 				position += diceNumber;
@@ -22,8 +22,11 @@ public class SnakeAndLadder {
 				if (position < 0) {
 					position = 0;
 				}
-				System.out.println("Snake Move to " + position);
+				System.out.println("Snake Return to " + position);
 			}
+			dicecount++;
+			
 		}
+		System.out.println("Dice Count: "+ dicecount);
 	}
 }
