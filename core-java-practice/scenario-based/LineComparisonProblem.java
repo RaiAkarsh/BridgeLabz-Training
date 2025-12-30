43482f3eb7e4776ffd27f94ef75856c8618cdd3a
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 10:59:51 +0530

    [akarsh] Add. uc1 length of the line

--- core-java-practice/scenario-based/LineComparisonProblem.java
+++ core-java-practice/scenario-based/LineComparisonProblem.java
@@ -1,7 +1,16 @@
+import java.util.*;
 public class LineComparisonProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		Scanner x = new Scanner(System.in);
+		System.out.println("Enter the co-ordinates for the line  ");
+		int x1 =x.nextInt();
+		int y1 =x.nextInt();
+		int x2 =x.nextInt();
+		int y2 =x.nextInt();
+		double lengthofline= Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
+		System.out.println(lengthofline);
 		
 
 	}
