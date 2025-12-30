2d8cb23014888474dc656cce6f0ae94fc95edabd
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 11:58:56 +0530

    [akarsh] Refractor. optimised Line Comaparison Problem

--- core-java-practice/scenario-based/LineComparisonProblem.java
+++ core-java-practice/scenario-based/LineComparisonProblem.java
@@ -3,30 +3,16 @@ public class LineComparisonProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		Scanner x = new Scanner(System.in);
-		System.out.println("Enter the co-ordinates for the line 1 ");
-		int x1 =x.nextInt();
-		int y1 =x.nextInt();
-		int x2 =x.nextInt();
-		int y2 =x.nextInt();
-		System.out.println("Enter the co-ordinates for the line 2 ");
-		int a1 =x.nextInt();
-		int b1 =x.nextInt();
-		int a2 =x.nextInt();
-		int b2 =x.nextInt();
-		//calculating length of line 1 and line 2 (uc1)
-		double lengthofline1= Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
-		double lengthofline2= Math.sqrt(Math.pow(a2 - a1,2) + Math.pow(b2 - b1,2));
-		String line1 = String.valueOf(lengthofline1);
-		String line2 = String.valueOf(lengthofline2);
-		//comparing two lines using equals method (uc2)
-		if(line1.equals(line2)) {
-			System.out.print("They are equal");
+		Double line1= findlength(1);
+		Double line2= findlength(2);
+		//comparing two lines using equals method 
+		if(Double.valueOf(line1).equals(line2)) {
+			System.out.println("They are equal");
 		}
 		else {
 			System.out.println("They are not equal");
 		}
-		//comparing two lines using compareTo method (uc3)
+		//comparing two lines using compareTo method 
 		int result = line1.compareTo(line2);
 		if(result < 0) {
 			System.out.println("Line 2 is greater than Line 1");
@@ -38,6 +24,18 @@ public class LineComparisonProblem {
 			System.out.println("Both lines are equal");
 		}
 
+	}
+	//calculating length of line 1 and line 2 
+	public static double findlength(int i) {
+		Scanner x =new Scanner(System.in);
+		System.out.println("Enter the co-ordinates for the line "+i);
+		int x1 =x.nextInt();
+		int y1 =x.nextInt();
+		int x2 =x.nextInt();
+		int y2 =x.nextInt();
+		double length= Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
+		System.out.println("Length of the line "+i+" is "+length);
+		return length;
 	}
 
 }
