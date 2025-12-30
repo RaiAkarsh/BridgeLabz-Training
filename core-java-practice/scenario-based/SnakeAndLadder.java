5a0ee80e3c4ff51dc9687787a315a4feeb7c9773
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 14:57:40 +0530

    [akarsh] Add. uc1 single player start at 0

---
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadder {
+	public static void main(String []args) {
+		System.out.println("Snake and Ladder game");
+		System.out.println("Start position is 0");
+		int position=0;
+	}
+
+}
