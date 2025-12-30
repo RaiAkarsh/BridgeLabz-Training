f974363e93a468d2a192045f276708bd2c2eb0ec
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 15:20:52 +0530

    [akarsh] Refactor. uc4 Repeat till player reaches winning 100 and make sure start point is always 0 and not below it

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -1,24 +1,29 @@
 import java.util.*;
+
 public class SnakeAndLadder {
-	public static void main(String []args) {
+	public static void main(String[] args) {
 		System.out.println("Snake and Ladder game");
 		System.out.println("Start position is 0");
-		int position=0;
-		
-		int diceNumber = (int)(Math.random()*6)+1;
-		System.out.println("Dice Number "+  diceNumber);
-		
-		int option = (int)(Math.random()*3)+1;
-		if(option==1) {
-			System.out.println("No Play ");
-		}
-		else if(option==2) {
-			position+=diceNumber;
-			System.out.println("Ladder Move to "+ position);
-		}
-		else {
-			position-=diceNumber;
-			System.out.println("Snake Move to "+ position);
+		int position = 0;
+		while (position < 100) {
+			int diceNumber = (int) (Math.random() * 6) + 1;
+			System.out.println("Dice Number " + diceNumber);
+
+			int option = (int) (Math.random() * 3) + 1;
+			if (option == 1) {
+				System.out.println("No Play ");
+			} 
+			else if (option == 2) {
+				position += diceNumber;
+				System.out.println("Ladder Move to " + position);
+			} 
+			else {
+				position -= diceNumber;
+				if (position < 0) {
+					position = 0;
+				}
+				System.out.println("Snake Move to " + position);
+			}
 		}
 	}
 }
