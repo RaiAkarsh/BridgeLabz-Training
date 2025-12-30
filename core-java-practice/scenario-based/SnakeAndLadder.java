1237f91b352779355b9fe8656fc7984121504940
Akarsh Rai <akarshrai1405@gmail.com>
2025-12-30 15:02:28 +0530

    [akarsh] Refactor. uc2 use random to generate dice number between 1 to 6

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -1,8 +1,12 @@
+import java.util.*;
 public class SnakeAndLadder {
 	public static void main(String []args) {
 		System.out.println("Snake and Ladder game");
 		System.out.println("Start position is 0");
 		int position=0;
+		
+		int diceNumber = (int)(Math.random()*6)+1;
+		System.out.println("Dice Number "+  diceNumber);
 	}
 
 }
