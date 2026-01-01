bffa63e1a2bd0b43dd7010d61aae50ed879481e8
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-01 15:26:51 +0530

    [akarsh] Add. Book Program

---
+++ oops-practice/gcr-codebase/this-static-final/Book.java
@@ -0,0 +1,29 @@
+class Book {
+    static String libraryName = "Egmore Library";
+    String title;
+    String author;
+    final String isbn; 
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+    static void displayLibraryName() {
+        System.out.println("Library Name: " + libraryName);
+    }
+    void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + isbn);
+    }
+    public static void main(String[] args) {
+        Book book1 = new Book(
+                "Effective Java","Joshua Bloch","978-0134685991");
+        Book.displayLibraryName();
+        if (book1 instanceof Book) {
+            book1.displayDetails();
+        } else {
+            System.out.println("Object is not a Book");
+        }
+    }
+}
