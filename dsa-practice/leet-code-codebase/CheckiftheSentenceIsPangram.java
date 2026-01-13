904e91e820538c2cc120fb434986c265d07b5451
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-13 23:17:12 +0530

    [akarsh] Add. Check if the Sentence Is Pangram

---
+++ dsa-practice/leet-code-codebase/CheckiftheSentenceIsPangram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class CheckiftheSentenceIsPangram {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter the sentence: ");
+        String sentence = x.nextLine();
+        if (checkIfPangram(sentence)) {
+            System.out.println("The sentence is a pangram.");
+        } else {
+            System.out.println("The sentence is not a pangram.");
+        }
+    }
+    public static boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) return false;
+        boolean[] seen = new boolean[26];
+        for (char c : sentence.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                seen[c - 'a'] = true;
+            }
+        }
+        for (boolean b : seen) {
+            if (!b) return false;
+        }
+        return true;
+    }
+
+}
