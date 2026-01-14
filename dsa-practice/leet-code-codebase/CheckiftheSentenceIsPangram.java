891de6d21aae2ff940def9ca47299c79c0bb71dc
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-14 23:45:59 +0530

    [akarsh] Update. Check if the Sentence Is Pangram

--- dsa-practice/leet-code-codebase/CheckiftheSentenceIsPangram.java
+++ dsa-practice/leet-code-codebase/CheckiftheSentenceIsPangram.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CheckiftheSentenceIsPangram {
+public class CheckIfTheSentenceIsPangram {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         System.out.println("Enter the sentence: ");
