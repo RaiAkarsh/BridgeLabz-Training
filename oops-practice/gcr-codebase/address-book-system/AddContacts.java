c754190b9ff0a59d0644bb976085125925192434
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-16 16:27:24 +0530

    [akarsh] Add. UC3 Ability to edit Contacts

--- oops-practice/gcr-codebase/address-book-system/AddContacts.java
+++ oops-practice/gcr-codebase/address-book-system/AddContacts.java
@@ -5,4 +5,40 @@ public class AddContacts {
     void addContact(Contact contact){
         contactList.add(contact);
     }
+    void editContact(String firstName, String field, String newValue) {
+        for (Contact contact : contactList) {
+            if (contact.getFirstName().equals(firstName)) {
+                switch (field.toLowerCase()) {
+                    case "firstname":
+                        contact.setFirstName(newValue);
+                        break;
+                    case "lastname":
+                        contact.setLastName(newValue);
+                        break;
+                    case "address":
+                        contact.setAddress(newValue);
+                        break;
+                    case "city":
+                        contact.setCity(newValue);
+                        break;
+                    case "state":
+                        contact.setState(newValue);
+                        break;
+                    case "zip":
+                        contact.setZip(newValue);
+                        break;
+                    case "phonenumber":
+                        contact.setPhoneNumber(newValue);
+                        break;
+                    case "email":
+                        contact.setEmail(newValue);
+                        break;
+                    default:
+                        System.out.println("Invalid field: " + field);
+                }
+                return; 
+            }
+        }
+        System.out.println("Contact with first name " + firstName + " not found.");
+    }
 }
--- oops-practice/gcr-codebase/address-book-system/AddressBookMain.java
+++ oops-practice/gcr-codebase/address-book-system/AddressBookMain.java
@@ -1,8 +1,30 @@
+import java.util.*;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        Contact contact=new Contact("John","Doe","123 Main St","Springfield","IL","62701","555-1234","tgtgt");
+        Scanner x=new Scanner(System.in);
+        System.out.println("Enter contact details:");
+        System.out.print("First Name: ");
+        String firstName=x.nextLine();
+        System.out.print("Last Name: ");
+        String lastName=x.nextLine();
+        System.out.print("Address: ");
+        String address=x.nextLine();
+        System.out.print("City: ");
+        String city=x.nextLine();
+        System.out.print("State: ");
+        String state=x.nextLine();
+        System.out.print("Zip: ");
+        String zip=x.nextLine();
+        System.out.print("Phone Number: ");
+        String phoneNumber=x.nextLine();
+        System.out.print("Email: ");
+        String email=x.nextLine();
+        
+        Contact contact=new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
         AddContacts addContacts=new AddContacts();
         addContacts.addContact(contact);
+        // edit the given contact based on first name , field to be edited and new value
+        addContacts.editContact("John","address","456 Elm St");
     }
 }
