1c5cf666b03c016c2c8dff72fa4c9b995a901107
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-16 15:40:19 +0530

    [akarsh] Add. UC2 Ability to add Contacts

---
+++ oops-practice/gcr-codebase/address-book-system/AddContacts.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AddContacts {
+    List<Contact> contactList=new ArrayList<>();
+
+    void addContact(Contact contact){
+        contactList.add(contact);
+    }
+}
--- oops-practice/gcr-codebase/address-book-system/AddressBookMain.java
+++ oops-practice/gcr-codebase/address-book-system/AddressBookMain.java
@@ -1,5 +1,8 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        Contact contact=new Contact("John","Doe","123 Main St","Springfield","IL","62701","555-1234","tgtgt");
+        AddContacts addContacts=new AddContacts();
+        addContacts.addContact(contact);
     }
 }
