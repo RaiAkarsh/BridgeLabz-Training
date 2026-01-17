15517166bf56a5094f042600f0436c19564e7ae0
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-17 22:14:06 +0530

    [akarsh] Add. File Reader Line by Line

---
+++ dsa-practice/gcr-codebase/search-string-file/FileReaderLineByLine.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class FileReaderLineByLine {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("sample.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line=br.readLine())!=null){
+                System.out.println(line);
+            }
+            br.close();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
