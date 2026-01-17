a500f56ba28b371ef713fca44ad0d5ffb2bc9e30
Akarsh Rai <akarshrai1405@gmail.com>
2026-01-17 22:13:57 +0530

    [akarsh] Add. Binary Search Rotation Point

---
+++ dsa-practice/gcr-codebase/search-string-file/BinarySearchRotationPoint.java
@@ -0,0 +1,15 @@
+public class BinarySearchRotationPoint {
+    public static void main(String[] args) {
+        int[] arr = {15, 18, 2, 3, 6, 12};
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        System.out.println("Rotation point index: " + left);
+    }
+}
