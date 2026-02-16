d635583ead4f8c7d31b92050d87bb6f3eeb8f60e
Akarsh Rai <akarshrai1405@gmail.com>
2026-02-16 11:44:26 +0530

    [akarsh] Refactor. UC3 and UC4 - Added part time and used switch case

--- java-8-features-practice/gcr-codebase/employee-wage-problem/EmployeeMain.java
+++ java-8-features-practice/gcr-codebase/employee-wage-problem/EmployeeMain.java
@@ -3,13 +3,22 @@ class EmployeeMain{
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computatio Program");
         int attendence = (int)(Math.random()*2);
-        if(attendence==0){
-            System.out.println("Employee is Absent");
-            System.out.println("Daily wage : "+0);
+        int fullTime=8,partTime=4,wages=20,dailywages=0;
+        if(attendence==1){
+            int typeOfWork= (int)(Math.random()*2);
+            switch(typeOfWork){
+                case 0:
+                    System.out.println("Full Time");
+                    dailywages=fullTime*wages;
+                    break;
+                case 1:
+                    System.out.println("Part Time");
+                    dailywages=partTime*wages;
+            }
+            System.out.println("Daily wages : " + dailywages);
         }
         else{
-            System.out.println("Employee is Present");
-            System.out.println("Daily wage : "+20*8);
+            System.out.println("Employee is absent");
         }
     }
 }
